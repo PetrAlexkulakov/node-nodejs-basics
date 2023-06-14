4bef77cdf5d7ebc6f6783d58b3070dc3b3c68727
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 10:38:15 +0400

    feat: create rename func

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = './src/fs/files/wrongFilename.txt';
+    const newPath = './src/fs/files/properFilename.md';
+  
+    if (!fs.existsSync(oldPath)) {
+      throw new Error('FS operation failed');
+    }
+  
+    if (fs.existsSync(newPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.renameSync(oldPath, newPath);
 };
 
 await rename();
