d91ffdea463c4b7fe8995e93f470acca0dbb2aa0
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 18:54:32 +0400

    feat: create write stream func

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import readline from 'readline';
+
 const write = async () => {
-    // Write your code here 
+    const ourPath = './src/streams/files/fileToWrite.txt';
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const writeStream = fs.createWriteStream(ourPath);
+
+    rl.on('line', (input) => {
+        writeStream.write(input + '\n');
+    });
+    
+    rl.on('close', () => {
+        writeStream.end();
+    });
 };
 
 await write();
