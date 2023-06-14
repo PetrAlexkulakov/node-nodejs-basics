0ceef946f023802d11f95ad7b79e0f13791156cc
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 10:41:43 +0400

    feat: create delete func

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const remove = async () => {
-    // Write your code here 
+    const ourPath = './src/fs/files/fileToRemove.txt';
+  
+    if (!fs.existsSync(ourPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    fs.unlinkSync(ourPath);
 };
 
 await remove();
