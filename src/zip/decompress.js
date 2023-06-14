3bd0a0d3c7b6ba63c74e8d39b8e9613f49aeb6bc
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 21:09:01 +0400

    feat: create decompress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = './src/zip/files/fileToCompress.txt';
+    const archivePath = './src/zip/archive.gz';
+
+    const readStream = fs.createReadStream(archivePath);
+    const writeStream = fs.createWriteStream(filePath);
+    readStream.pipe(zlib.createGunzip()).pipe(writeStream);
+  
+    await new Promise((resolve, reject) => {
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
+    });
 };
 
 await decompress();
