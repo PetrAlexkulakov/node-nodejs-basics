a233c3795cd915fa456c433752fce4f4d5f629f8
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 10:47:48 +0400

    feat: create read func

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const ourPath = './src/fs/files/fileToRead.txt';
+  
+    if (!fs.existsSync(ourPath)) {
+      throw new Error('FS operation failed');
+    }
+
+    console.log(fs.readFileSync(ourPath, 'utf8'));
 };
 
 await read();
