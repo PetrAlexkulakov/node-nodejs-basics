bf5d644dff137db824eeae6c79f984846d51cace
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 20:50:10 +0400

    feat: create transform func

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { Transform } from 'stream';
+import readline from 'readline';
+
 const transform = async () => {
-    // Write your code here 
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const reverseTransform = new Transform({
+        transform(chunk) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            this.push(reversedChunk);
+        },
+    });
+    
+    rl.on('line', (input) => {
+        reverseTransform.write(input + '\n');
+    });
+
+    rl.on('close', () => {
+        reverseTransform.end();
+    });
+
+    reverseTransform.pipe(process.stdout);
 };
 
 await transform();
