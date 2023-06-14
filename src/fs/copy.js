562b56ae742f7e93364120ad43f7c5cd0cb90df9
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 09:56:14 +0400

    feat: create copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const sourceFolderPath = './src/fs/files';
+  const destinationFolderPath = './src/fs/files_copy';
+
+  if (!fs.existsSync(sourceFolderPath)) {
+    throw new Error('FS operation failed');
+  }
+
+  if (fs.existsSync(destinationFolderPath)) {
+    throw new Error('FS operation failed');
+  }
+
+  fs.mkdirSync(destinationFolderPath);
+
+  const files = fs.readdirSync(sourceFolderPath);
+
+  files.forEach((file) => {
+    const sourceFilePath = path.join(sourceFolderPath, file);
+    const destinationFilePath = path.join(destinationFolderPath, file);
+
+    fs.copyFileSync(sourceFilePath, destinationFilePath);
+  });
 };
 
-await copy();
+await copy();//try it
