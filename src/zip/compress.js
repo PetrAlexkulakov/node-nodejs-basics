5e8c476adf04af4b2fdebadc58621e381848d22e
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-14 21:00:34 +0400

    feat: create compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = './src/zip/files/fileToCompress.txt';
+    const outputFilePath = './src/zip/archive.gz';
+
+    const readableStream = fs.createReadStream(inputFilePath);
+    const writableStream = fs.createWriteStream(outputFilePath);
+
+    const gzipStream = zlib.createGzip();
+
+    readableStream.pipe(gzipStream).pipe(writableStream);
+
+    await new Promise((resolve, reject) => {
+        writableStream.on('finish', resolve);
+        writableStream.on('error', reject);
+    });
 };
 
 await compress();
