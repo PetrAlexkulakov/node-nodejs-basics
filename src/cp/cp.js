d374ff449eda6683375a4fa4ae26fa5e12a14e4b
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-06-17 19:48:43 +0400

    feat: create cp func

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,10 @@
+import { fork } from 'child_process'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork('./src/cp/files/script.js', args, {
+        stdio: [process.stdin, process.stdout, 'ipc']
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  ['someArgument1', 'someArgument2'] );
+// './src/cp/files/script.js'
